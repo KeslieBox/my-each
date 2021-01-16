ae72ff48064e03793de2c4bed3d2acf79bca09b8
KeslieBox <kesliebox@gmail.com>
2021-01-15 21:40:22 -0600

    commit tree df5a4a29068c72508b96d9811b5afd4e2e0c595c

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+# accept an argument of an array
+def my_each (array) 
   # code here
+  # use the `while` loop to iterate over each member of that array
+  i = 0
+  while i < array.length
+  #  yielding each element contained in the array to a block.
+    yield(array[i])
+    i = i + 1
+  end
+  array
 end
